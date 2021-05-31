c7362b22a52933c211fe03dc8783251d9c75d0f5
Mihnea Mihnea <tmihnea555@gmail.com>
2021-05-31 11:34:41 +0300

    home page deployed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,10 @@
 import { React, useState } from 'react';
 import styles from './About.module.scss';
 import Transition from '../Transition/Transition';
-import Avatar from '../UI/Avatar';
 import Skills from './Skills';
-import Typing from 'react-typing-animation';
-import Scroll from '../UI/Scroll';
 import { useSpring, animated } from 'react-spring';
-import LimitBar from '../UI/LimitBar';
-
-
+import backgroundVideo from "../video/typing_video.mp4";
+import { Player } from 'video-react';
 //STATIC list of objects witch contains skills with title, image and description
 const skills = [
     { id: 0, skillName: 'HTML', image: 'fab fa-html5', text: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.' },
@@ -21,81 +17,12 @@ const skills = [
 ]
 
 const About = () => {
-    const [skill, setSkill] = useState(skills);
-
-    //variables for "string tags" which I can use inside jsx tags
-    const aboutOpenTag = '<About>';
-    const aboutClosedTag = '</About>';
-    const mySkillsOpenTag = '<Main skills>';
-    const mySkillsClosedTag = '</Main skills>';
-    const languageOpenTag = '<Languages>';
-    const languageClosedTag = '</Languages>';
-
-    return (
-        <div>
-            <Transition />
-
-            <div className={styles.about}>
-                <div className={styles.resume}>
-                    <a href="https://github.com/WeissDotEXE" target="_blank"><button><i class="fab fa-github"></i></button></a>
-                    <button id={styles.resumeBtn}><i class="fas fa-download"></i>Resume</button>
-                    <a href="https://www.linkedin.com/in/mihnea-cristian-tache-022a2b202/" target="_blank"><button><i class="fab fa-linkedin"></i></button></a>
-                </div>
-                <Typing startDelay={1600} speed={30} >
-                    <div className={styles.aboutHeader} >
-                        <p>{aboutOpenTag}</p>
-                    </div>
-                </Typing>
-                <div className={styles.aboutText}>
-                    <p id={styles.aboutTxt}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                </div>
-                <Typing startDelay={2000} speed={30}>
-                    <div className={styles.aboutHeader} >
-                        <p>{aboutClosedTag}</p>
-                    </div>
-                </Typing>
-                <LimitBar />
-
-                <div className={styles.avatar}>
-                    <Avatar />
-                </div>
-
-
-                <Typing startDelay={2600} speed={30}>
-                    <div className={styles.aboutHeader} >
-                        <p>{mySkillsOpenTag}</p>
-                    </div>
-                </Typing>
-                <Skills items={skill} />
-                <div className={styles.aboutHeader} >
-                    <p>{mySkillsClosedTag}</p>
-                </div>
-                <LimitBar />
-
-                <div>
-                    <div className={styles.aboutHeader} >
-                        <p>{languageOpenTag}</p>
-                    </div>
-                    <div className={styles.languages}>
-                        <table>
-                            <tr>
-                                <th>// Native</th>
-                                <th>// Advanced</th>
-                                <th>// Basic</th>
-                            </tr>
-                            <tr>
-                                <td>Romanian</td>
-                                <td>English</td>
-                                <td>Chinese</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div className={styles.aboutHeader} >
-                        <p>{languageClosedTag}</p>
-                    </div>
-                </div>
-                <Scroll />
-            </div>
+    const videoSrc=backgroundVideo;
+    return(
+        <div className={styles.about}>
+            <video autoplay loop id={styles.videoo}>
+                <source src={backgroundVideo}/>
+            </video>
         </div>
     );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,63 +3,34 @@ import styles from './Home.module.scss';
 import Transition from '../Transition/Transition';
 import image from '../Images/web-dev.svg';
 import { useSpring, animated } from 'react-spring';
-
+import { Link } from 'react-router-dom';
+import avatar from '../Images/avatar.jpg';
+import logo from '../Images/logo wde.png';
 const Home = () => {
 
-    const nameAnime=useSpring({
-        delay:2400,
-        to:{opacity:1,transform:`translateX(0px)`},
-        from:{opacity:0,transform:`translateX(-110px)`},
-        config:{duration:500}
-    });
-    const jobAnime=useSpring({
-        delay:2600,
-        to:{opacity:1,transform:`translateX(0px)`},
-        from:{opacity:0,transform:`translateX(-110px)`},
-        config:{duration:600}
-    })
-    const contactAnime=useSpring({
-        delay:2800,
-        to:{opacity:1,transform:`translateX(0px)`},
-        from:{opacity:0,transform:`translateX(-110px)`},
-        config:{duration:600}
-    })
-    const imageAnime=useSpring({
-        delay:3000,
-        to:{opacity:1},
-        from:{opacity:0},
-        config:{duration:500}
-    })
 
     return (
         <div>
             <Transition />
-            <div className={styles.presentation}>
-                <div className={styles.leftPresentation}>
-                    <animated.div style={nameAnime} className={styles.titleLetter}>
-                        <ul className={styles.letters}>
-                            <li id={styles.w}>W</li>
-                            <li id={styles.e1}>e</li>
-                            <li id={styles.i}>i</li>
-                            <li id={styles.s1}>s</li>
-                            <li id={styles.s2}>s</li>
-                            <li id={styles.d}>D</li>
-                            <li id={styles.o}>o</li>
-                            <li id={styles.t}>t</li>
-                            <li id={styles.e2}>E</li>
-                            <li id={styles.x}>x</li>
-                            <li id={styles.e3}>E</li>
-                        </ul>
-                    </animated.div>
-                    <animated.div style={jobAnime} className={styles.jobs}>
-                        <h3 id={styles.job1}>Front End Developer </h3>
-                        <h3 id={styles.job2}>UI Designer</h3>
-                    </animated.div>
-                    <animated.button style={contactAnime} id={styles.contactBtn}>Contact me</animated.button>
-                </div>
-                <animated.div style={imageAnime} className={styles.rightPresentation}>
-                    <img id={styles.image} src={image} />
-                </animated.div>
+            <div className={styles.home}>
+                <h1 id={styles.titlePhone}>WeissDotExe</h1>
+                <Link to="/"><img id={styles.navbarLogo} src={logo} alt="logo"/></Link>
+                
+                <Link to="/about" className={styles.leftSide}>
+                    <div className={styles.leftSide}>
+                        <h1 id={styles.aboutTxt}>About me</h1>
+                        <img id={styles.leftImage} src={avatar} />
+                    </div>
+                </Link>
+
+                <div className={styles.blankSpace}></div>
+                
+                <Link to="/portofolio" className={styles.rightSide}>
+                    <div className={styles.rightSide}>
+                        <h1 id={styles.portofolioTxt}>Portofolio</h1>
+                        <img id={styles.rightImage} src="https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" />
+                    </div>
+                </Link>
             </div>
         </div>
     );
