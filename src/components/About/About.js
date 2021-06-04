0b424385089e54d376f6dba8466cd168f823aab1
Mihnea Mihnea <tmihnea555@gmail.com>
2021-06-04 14:23:18 +0300

    home and about pages completed for desktop

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,15 @@
 import { React, useState } from 'react';
 import styles from './About.module.scss';
 import Transition from '../Transition/Transition';
-import Skills from './Skills';
+
 import { useSpring, animated } from 'react-spring';
-import backgroundVideo from "../video/typing_video.webm";
-import { Player } from 'video-react';
+//import images
+import avatar1 from '../Images/avatar.jpg';
+import avatar2 from '../Images/avatar2.jpg';
+import lights from '../Images/lights.jpg';
+import exam from '../Images/exam.svg';
+import Skills from './Skills';
+
 //STATIC list of objects witch contains skills with title, image and description
 const skills = [
     { id: 0, skillName: 'HTML', image: 'fab fa-html5', text: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.' },
@@ -15,14 +20,71 @@ const skills = [
     { id: 5, skillName: 'React', image: 'fab fa-react', text: 'React is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook' },
     { id: 6, skillName: 'Figma', image: 'fab fa-figma', text: 'Figma is a vector graphics editor and prototyping tool which is primarily web-based' },
 ]
-
 const About = () => {
-    const videoSrc=backgroundVideo;
     return(
-        <div className={styles.about}>
-            <video autoplay loop id={styles.videoo}>
-                <source src={backgroundVideo}/>
-            </video>
+        <div>
+            <Transition />
+            <div className={styles.about}>
+                <div className={styles.intro}>
+
+                    <h1 id={styles.intro_txt}>About me</h1>
+                    <div className={styles.intro_btn}>
+                        
+                        <a target="_blank" href="https://github.com/WeissDotEXE">
+                            <i class="fab fa-github"></i>
+                        </a>
+
+                        <a target="_blank" href="#">
+                            <button>Resume</button>
+                        </a>
+
+                        <a target="_blank" href="https://github.com/WeissDotEXE">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div className={styles.presentation}>
+                    <h1 data-aos="fade-up" id={styles.first_txt}>Hi. My name is Mihnea</h1>
+                    <h1 data-aos="fade-up" id={styles.second_txt}>Front-End Web Developer</h1>
+                    <img data-aos="fade-up" data-aos-offset="200" id={styles.avatar2} src={avatar2}/>
+                    <img data-aos="fade-up" data-aos-offset="200" id={styles.avatar1} src={avatar1}/>
+                </div>
+
+                <div  className={styles.studies}>
+                    <h1 data-aos="fade-right" id={styles.studies_txt}>Currently, I am student at Faculty of Cybernetics, Statistics and Informatics Bucharest , Romania</h1>
+                    <h1 data-aos="fade-right" id={styles.studies_txt2}>I am also a member of Microsoft Learn Student Ambassadors program , in Tech Department.</h1>
+                    <img src={exam}/>
+                </div>
+                
+                <div className={styles.learning}>
+                    <div id={styles.image_overlay}></div>
+                    <img  src={lights}/>
+                    <h1 data-aos="fade-right" id={styles.learning_txt1}>I started learning web development in the middle of 2020 due to boredom.</h1>
+                    <h1 data-aos="fade-right" id={styles.learning_txt2}>Now , all my free time is invested in upgrading my web developer skills.</h1>
+                </div>
+
+                <div className={styles.so_now}>
+                    <div class={styles.so_now_txt}>
+                        <div id={styles.left_line}></div>
+                        <h1 data-aos="fade-up" data-aos-offset="200">So now , you are able to see this website.</h1>
+                    </div>
+                    <iframe data-aos="fade-up" data-aos-offset="200" id={styles.gif} src="https://codetraveler.io/content/images/2019/05/giphy.gif"/>
+                </div>
+
+                <div className={styles.skills}>
+                    <Skills />
+                </div>
+
+                <div className={styles.languages}>
+                    <h1>Languages</h1>
+                    <div className={styles.languages_txt}>
+                        <p>Native: romanian</p>
+                        <p>Advanced: english</p>
+                        <p>Basic: chinese</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import styles from './Skills.module.scss';
-import SkillsItem from './SkillsItem';
-const Skills = (props) => {
+import React from "react";
+import styles from "./Skills.module.scss";
+import Card from "../UI/Card";
 
-    return (
-        <div className={styles.skills}>
-            <ul className={styles.skillsList}>
-                {props.items.map((skills) => (
-                    <SkillsItem
-                        key={skills.id}
-                        name={skills.skillName}
-                        image={skills.image}
-                        text={skills.text} />
-                ))}
-            </ul>
-        </div>
-    );
-}
+//STATIC list of objects witch contains skills with title, image and description
+const hardSkills = ['Html', 'Css','Bootstrap','Javascript', 'React', 'Figma'];
+const softSkills = ['Teamwork' , 'Patience', 'Hardworking' , 'Friendliness', 'Confident' , 'Desire to learn'];
+const softSkillName=softSkills.map((sSkill=><p data-aos="fade-up">{sSkill}</p>));
+const hardSkillName=hardSkills.map((hSkill=><p data-aos="fade-up">{hSkill}</p>));
+
+const Skills = (props) => {
+  return (
+    <div className={styles.skill_card}>
+      <Card>
+        <h1 id={styles.skills_txt}>Soft Skills</h1>
+        <i class="fas fa-user-friends"></i>
+        {softSkillName}
+      </Card>
+      <Card id={styles.card2}>
+        <h1 id={styles.skills_txt}>Hard skills</h1>
+        <i class="fas fa-pencil-ruler"></i>
+        {hardSkillName}
+      </Card>
+    </div>
+  );
+};
 
 export default Skills;
