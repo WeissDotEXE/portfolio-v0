fd267bf6dad9b918dbd6990aa83696a148347d1c
Mihnea Mihnea <tmihnea555@gmail.com>
2021-06-08 21:15:25 +0300

    contact page for mobile update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,8 +5,17 @@ import background from "../Images/contact-background.jpg";
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>
+    <div>
+      <Transition />
+      <div className={styles.contact}>
       <img src={background} />
+      <div className={styles.text}>
+        <h1>Have a question or want to work together?</h1>
+        <div className={styles.email}>
+          <i class="far fa-envelope"></i>
+          <p>futuremail@gmail.com</p>
+        </div>
+      </div>
       <form>
         <label>Full Name</label>
         <input
@@ -29,19 +38,12 @@ const Contact = () => {
         <button type="submit">Send</button>
       </form>
 
-      <div className={styles.text}>
-        <h1>Have a question or want to work together?</h1>
-        <div className={styles.email}>
-          <i class="far fa-envelope"></i>
-          <p>futuremail@gmail.com</p>
-        </div>
-      </div>
-
       <div className={styles.social}>
         <h1 id={styles.social_txt}>Social</h1>
         <a href="https://www.linkedin.com/in/mihnea-cristian-tache-022a2b202/" target="_blank"><i class="fab fa-linkedin"></i></a>
         <a href="https://www.linkedin.com/in/mihnea-cristian-tache-022a2b202/" target="_blank"><i class="fab fa-instagram"></i></a>
       </div>
+    </div>
     </div>
   );
 };
