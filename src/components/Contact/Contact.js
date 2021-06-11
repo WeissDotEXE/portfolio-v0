10f63a853c54589cf672a16e4d224748d322e408
Mihnea Mihnea <tmihnea555@gmail.com>
2021-06-11 18:09:48 +0300

    fixed bugs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,26 @@ import styles from "./Contact.module.scss";
 import Transition from "../Transition/Transition";
 import background from "../Images/contact-background.jpg";
 
+//import emailjs for emails
+import emailjs from "emailjs-com";
 const Contact = () => {
+
+  const sendEmail=e=>{
+    e.preventDefault();
+
+    emailjs.sendForm('service_v0d8hrp', 'template_7dxngpv', e.target, 'user_rJmuWbZU1IbW3CGt9OsXm')
+      .then((result) => {
+          return(
+            <div className={styles.succes_email}>
+              <h1>Awesome , I will read your message soon !</h1>
+            </div>
+          );
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
+
   return (
     <div>
       <Transition />
@@ -16,21 +35,27 @@ const Contact = () => {
           <p>futuremail@gmail.com</p>
         </div>
       </div>
-      <form>
+      <form onSubmit={sendEmail}>
         <label>Full Name</label>
         <input
+        required
+          name="name"
           id={styles.simple_input}
           type="text"
           placeholder="Sebastian John"
         />
         <label>Email</label>
         <input
+        required
+        name="email"
           id={styles.simple_input}
           type="email"
           placeholder="sebastianjohn@test.com"
         />
         <label>Message</label>
         <textarea
+        required
+          name="message"
           id={styles.msg_input}
           type="message"
           placeholder="I just wanna say you are awesome!"
